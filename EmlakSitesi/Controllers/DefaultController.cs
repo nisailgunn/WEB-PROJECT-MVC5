f76ded22a0b5d856b2e96837cd04ef60a675c5c0
nisailgunn <nisailgyn@gmail.com>
2021-09-29 12:53:57 +0300

    MVC5 Web Projesi

---
+++ EmlakSitesi/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmlakSitesi.Models.Entity;
+
+namespace EmlakSitesi.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        dbemlakEntities db = new dbemlakEntities();
+        public ActionResult Index()
+        {
+            var deger = db.TBL_EVLER.ToList();
+            return View(deger);
+        }
+    }
+}
